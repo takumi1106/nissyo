9d8578aa418bce4b96f292ed7baef58712698157
takumi1106 <takumi.g.116@gmail.com>
2025-12-24 18:37:10 +0900

    修正

--- js/script.js
+++ js/script.js
@@ -30,30 +30,7 @@ fetch("api/news.php")
         console.error(err);
         document.getElementById("newsList").innerHTML = "<li>ニュースを取得できませんでした</li>";
     });
-//==========================ロード画面============================//
-// window.addEventListener('load', function () {
-//     const loadingScreen = document.getElementById('loading-screen');
-//     const loadingText = document.querySelector('.loading-text');
 
-//     // 最低表示時間 0.55秒
-//     const minDisplay = 200;
-
-//     setTimeout(() => {
-//         // 文字をフェードアウト
-//         loadingText.style.transition = 'opacity 2.3s ease';
-//         loadingText.style.opacity = 0;
-
-//         setTimeout(() => {
-//             // 背景をフェードアウト
-//             loadingScreen.style.transition = 'opacity 0.5s ease';
-//             loadingScreen.style.opacity = 0;
-
-//             setTimeout(() => {
-//                 loadingScreen.style.display = 'none';
-//             }, 700);
-//         }, 700);
-//     }, minDisplay);
-// });
 //========================== ヘッダー ============================//
 let lastScroll = 0;
 
